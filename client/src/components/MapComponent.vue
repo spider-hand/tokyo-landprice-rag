266157f2467589296d5674c276d88b146395df91
spider-hand <creative.spider.hand@gmail.com>
2025-12-29 22:51:44 +0900

    feat: render land price layer

--- client/src/components/MapComponent.vue
+++ client/src/components/MapComponent.vue
@@ -6,9 +6,15 @@
 import { onMounted, onUnmounted, ref } from 'vue'
 import maplibregl from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css'
+import * as pmtiles from 'pmtiles'
+
+const COLOR_PALETTE = ['#ffffb2', '#fecc5c', '#fd8d3c', '#f03b20', '#bd0026']
 
 const mapContainer = ref<HTMLElement | null>(null)
 let map: maplibregl.Map | null = null
+
+const protocol = new pmtiles.Protocol()
+maplibregl.addProtocol('pmtiles', protocol.tile)
 
 onMounted(() => {
   if (mapContainer.value) {
@@ -21,6 +27,40 @@ onMounted(() => {
     })
 
     map.addControl(new maplibregl.NavigationControl(), 'bottom-left')
+
+    map.on('load', () => {
+      map!.addSource('land-price', {
+        type: 'vector',
+        url: 'pmtiles://http://localhost:9000/tokyo-landprice-rag/land_price.pmtiles',
+      })
+
+      map!.addLayer({
+        id: 'land-price-layer',
+        type: 'circle',
+        source: 'land-price',
+        'source-layer': 'land_price',
+        paint: {
+          'circle-radius': 5,
+          'circle-color': [
+            'step',
+            ['get', 'L01_006'],
+            COLOR_PALETTE[0] as string,
+            215000,
+            COLOR_PALETTE[1] as string,
+            370000,
+            COLOR_PALETTE[2] as string,
+            579000,
+            COLOR_PALETTE[3] as string,
+            1040000,
+            COLOR_PALETTE[4] as string,
+          ],
+          'circle-opacity': 0.8,
+          'circle-stroke-width': 1,
+          'circle-stroke-color': '#ffffff',
+          'circle-stroke-opacity': 0.2,
+        },
+      })
+    })
   }
 })
 
