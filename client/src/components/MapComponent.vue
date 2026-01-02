d357407d0de1031547b784fba1010cc88c11dbfe
spider-hand <creative.spider.hand@gmail.com>
2026-01-02 18:56:15 +0900

    chore: remove MinIO and serve PMTiles from client

--- client/src/components/MapComponent.vue
+++ client/src/components/MapComponent.vue
@@ -32,7 +32,7 @@ onMounted(() => {
       map!.addSource('land-price', {
         type: 'vector',
         // @see: https://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-L01-2025.html
-        url: 'pmtiles://http://localhost:9000/tokyo-landprice-rag/L01-25_13.pmtiles',
+        url: 'pmtiles:///data/L01-25_13.pmtiles',
       })
 
       map!.addLayer({
