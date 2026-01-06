492e2bc3f644d988ec31e3a52bfafb3b29e4f8a7
spider-hand <creative.spider.hand@gmail.com>
2026-01-07 01:06:06 +0900

    feat: replace quantile-based tiers with percentile-based ranking

--- client/src/components/MapComponent.vue
+++ client/src/components/MapComponent.vue
@@ -8,7 +8,14 @@ import maplibregl from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css'
 import * as pmtiles from 'pmtiles'
 
-const COLOR_PALETTE = ['#ffffb2', '#fecc5c', '#fd8d3c', '#f03b20', '#bd0026']
+const COLOR_PALETTE = [
+  'rgb(153, 102, 255)',
+  'rgb(54, 162, 235)',
+  'rgb(75, 192, 192)',
+  'rgb(255, 205, 86)',
+  'rgb(255, 159, 64)',
+  'rgb(255, 99, 132)',
+]
 
 const mapContainer = ref<HTMLElement | null>(null)
 let map: maplibregl.Map | null = null
@@ -46,14 +53,16 @@ onMounted(() => {
             'step',
             ['get', 'L01_008'],
             COLOR_PALETTE[0] as string,
-            228800,
+            35590,
             COLOR_PALETTE[1] as string,
-            409600,
+            149000,
             COLOR_PALETTE[2] as string,
-            666400,
+            514500,
             COLOR_PALETTE[3] as string,
-            1250000,
+            2520000,
             COLOR_PALETTE[4] as string,
+            17287000,
+            COLOR_PALETTE[5] as string,
           ],
           'circle-opacity': 0.8,
           'circle-stroke-width': 1,
