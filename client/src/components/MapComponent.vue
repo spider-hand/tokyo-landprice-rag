c00e1816e68a2ee9e02c2c43202d688a401d4b05
spider-hand <creative.spider.hand@gmail.com>
2026-01-07 20:46:54 +0900

    feat: add legends

--- client/src/components/MapComponent.vue
+++ client/src/components/MapComponent.vue
@@ -1,5 +1,19 @@
 <template>
-  <div ref="mapContainer" class="h-full w-full" />
+  <div class="relative h-full w-full">
+    <div ref="mapContainer" class="h-full w-full" />
+    <div class="bg-background absolute bottom-4 left-4 rounded-lg p-2 shadow-lg">
+      <div class="flex items-end gap-2 px-2 mb-1">
+        <div class="font-semibold">Land Price in 2025</div>
+        <div class="text-sm">(JPY per square meter)</div>
+      </div>
+      <ItemGroup>
+        <Item v-for="(item, index) in legendItems" :key="index" class="flex items-center px-2 py-1">
+          <div class="h-2 w-2 rounded-full" :style="{ backgroundColor: item.color }" />
+          <span>{{ item.label }}</span>
+        </Item>
+      </ItemGroup>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -7,6 +21,8 @@ import { onMounted, onUnmounted, ref } from 'vue'
 import maplibregl from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css'
 import * as pmtiles from 'pmtiles'
+import ItemGroup from './ui/item/ItemGroup.vue'
+import Item from './ui/item/Item.vue'
 
 const COLOR_PALETTE = [
   'rgb(153, 102, 255)',
@@ -15,6 +31,15 @@ const COLOR_PALETTE = [
   'rgb(255, 205, 86)',
   'rgb(255, 159, 64)',
   'rgb(255, 99, 132)',
+]
+
+const legendItems = [
+  { color: COLOR_PALETTE[5], label: 'Top 1% (> ¥17,287,000)' },
+  { color: COLOR_PALETTE[4], label: 'Top 10% (¥2,520,000 - ¥17,287,000)' },
+  { color: COLOR_PALETTE[3], label: 'Top 50% (¥514,500 - ¥2,520,000)' },
+  { color: COLOR_PALETTE[2], label: 'Bottom 50% (¥149,000 - ¥514,500)' },
+  { color: COLOR_PALETTE[1], label: 'Bottom 10% (¥35,590 - ¥149,000)' },
+  { color: COLOR_PALETTE[0], label: 'Bottom 1% (< ¥35,590)' },
 ]
 
 const mapContainer = ref<HTMLElement | null>(null)
@@ -32,8 +57,6 @@ onMounted(() => {
       center: [139.6917, 35.6895], // Tokyo
       zoom: 10,
     })
-
-    map.addControl(new maplibregl.NavigationControl(), 'bottom-left')
 
     map.on('load', () => {
       map!.addSource('land-price', {
