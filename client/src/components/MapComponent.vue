08071d981357ee35931b1c77e45dcd967aa46512
spider-hand <creative.spider.hand@gmail.com>
2026-01-10 22:14:39 +0900

    refactor: make PMTiles URL configurable

--- client/src/components/MapComponent.vue
+++ client/src/components/MapComponent.vue
@@ -96,8 +96,7 @@ onMounted(() => {
     map.on('load', () => {
       map!.addSource('land-price', {
         type: 'vector',
-        // @see: https://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-L01-2025.html
-        url: 'pmtiles:///data/L01-25_13.pmtiles',
+        url: import.meta.env.VITE_PMTILES_URL,
       })
 
       map!.addLayer({
