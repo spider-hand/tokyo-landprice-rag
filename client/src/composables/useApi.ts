70450da5e3ba0d55ea0cb2b6fa5f2994fbbff562
spider-hand <creative.spider.hand@gmail.com>
2026-01-11 18:22:14 +0900

    chore: configure rate limit on gateway

--- client/src/composables/useApi.ts
+++ client/src/composables/useApi.ts
@@ -3,6 +3,9 @@ import { Configuration } from '@/services'
 const useApi = () => {
   const apiConfig = new Configuration({
     basePath: import.meta.env.VITE_API_BASE_URL,
+    headers: {
+      'x-api-key': import.meta.env.VITE_PUBLIC_API_KEY,
+    },
   })
 
   return { apiConfig }
